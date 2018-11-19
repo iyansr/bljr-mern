5982a1c7142ac9d3023c37e935103681e8eb96d0
iyansr <ikhyan7@gmail.com>
2018-11-19 20:13:10 +0800

    add body parser

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -7,24 +8,32 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
-//DB Config
-// const db = require('./config/keys.js').mongoURI;
+//body-parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 
 //Connect to local mongoose
 mongoose
-    .connect('mongodb://localhost:27017/devcon', {useNewUrlParser:true})
-    .then( ()=> console.log('Mongodb Local Connected'))
-    .catch(err => console.log(err));
+.connect('mongodb://localhost:27017/devcon', {useNewUrlParser:true})
+.then(()=> console.log('Mongodb Local Connected'))
+.catch(err => console.log(err));
+
 
 //connect to mlab mongoose
+// DB Config
+// const db = require('./config/keys.js').mongoURI;
 // mongoose
 //     .connect(db, {useNewUrlParser: true})
 //     .then(()=> console.log('Mongod MLAB Connected'))
 //     .catch(err => console.log(err));
 
+
 app.get('/',(req,res)=>{
     res.send('Hello Kappa');
 });
+
+
 
 //Use Routes
 app.use('/api/users', users);
